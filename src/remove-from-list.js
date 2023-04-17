5645621654a31ac8ea781582b59ddc4025778d16
Oleg Preed <leo.silveri@mail.ru>
2023-04-18 02:05:48 +0700

    feat: wrote my implementations to the tasks Tue, Apr 18, 2023  2:05:48 AM

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,11 +22,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let node = l;
+  while (node) {
+    while (node.next !== null && node.next.value === k) {
+      node.next = node.next.next;
+    }
+    node = node.next;
+  }
+  if (l.value === k) {
+    l = l.next;
+  }
+  return l;
 }
 
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
